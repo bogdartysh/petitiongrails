673ef6f8f10ac13a50cc3d334a0ea5c6abf217b6
Bogdan Artyushenko <bogdan.artyushenko@gmail.com>
2014-12-07 21:50:48 +0200

    initial data value

--- petitionsweb/grails-app/conf/BootStrap.groovy
+++ petitionsweb/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,18 @@
+import org.petitions.Person
+
 class BootStrap {
 
-    def init = { servletContext ->
+    def init = { servletContext -> 
+		Person igor = new Person(firstName:"Ігор", secondName:"Андрійович", lastName: "Артюшенко", email:"artyushenko.igor@facebook.com", title:"Народний депутат України", personalPage:"http://artyushenko.com.ua", imageUrl:"http://www.artyushenko.com.ua/wp-content/uploads/2014/11/igor-artyushenko-logo.png")
+		igor.save(flush:true, failOnError:true)
+		
+		/**
+		 * Комітет з питань запобігання та протидії корупції
+		 */
+		
+		
+		
+    
     }
     def destroy = {
     }
