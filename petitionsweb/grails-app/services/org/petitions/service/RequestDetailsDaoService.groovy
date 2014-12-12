a4c6a2d8558829cf92b464d72b9549a7cd7f2599
Bogdan Artyushenko <bogdartysh@users.noreply.github.com>
2014-12-12 17:46:16 +0200

    better security

--- petitionsweb/grails-app/services/org/petitions/service/RequestDetailsDaoService.groovy
+++ petitionsweb/grails-app/services/org/petitions/service/RequestDetailsDaoService.groovy
@@ -7,7 +7,7 @@ import org.petitions.*
 class RequestDetailsDaoService {
 	def getPersistedRequestDetails(request) {
 		def newRequestDetails = new RequestDetails(remoteAddr :  ("" + request.getHeader("X-Forwarded-For")  + request.getRemoteAddr()).encodeAsSHA256(),
-		userAgent : ("" + request.getHeader("User-Agent")).encodeAsSHA256Bytes())
+		userAgent : ("" + request.getHeader("User-Agent")).encodeAsSHA256())
 		def oldRequestDetails = RequestDetails.findAll{
 			userAgent == newRequestDetails.userAgent && remoteAddr == newRequestDetails.remoteAddr
 		}
